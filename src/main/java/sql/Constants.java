d4a2059893fec60f8713f5bc6666749c6f9d8848
AGJ <86319979+Amalg016@users.noreply.github.com>
2023-06-23 16:23:56 +0530

    constants

--- src/main/java/sql/Constants.java
+++ src/main/java/sql/Constants.java
@@ -2,6 +2,6 @@ package sql;
 
 public class Constants {
 	public static String username = "root";
-	public static String pass = "pass";
+	public static String pass = "root@123";
 	public static String path = "jdbc:mysql://localhost:3306/scram";
 }
