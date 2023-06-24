faeed4a60ba3eae694a7bca20f0f7bf1a6f04a98
AGJ <86319979+Amalg016@users.noreply.github.com>
2023-06-24 11:36:36 +0530

    json conversion
    user object is been converted to json and is being displayed

--- src/main/java/com/signuppage/SignInServlet.java
+++ src/main/java/com/signuppage/SignInServlet.java
@@ -1,7 +1,46 @@
 package com.signuppage;
 
-import sql.SqlCommand;
+import java.io.IOException;
+import java.sql.SQLException;
 
-public class SignInServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import sql.SqlCommand;
+import sql.User;
+
+public class SignInServlet extends HttpServlet{
 	
+	@Override
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		String email = req.getParameter("email");
+		String pass = req.getParameter("password");
+        try {
+			User user=SqlCommand.FetchUser(email, pass);
+			
+			if(user != null) {
+//				 String json = new Gson().toJson(user);
+//				    
+				String json = "{ \"name\": \"" + user.uname + "\", "
+		                + "\"email\": \"" + user.uemail + "\", "
+		                + "\"dob\": \"" + user.udob + "\", "
+		                + "\"password\": \"" + user.upass + "\", "
+		                + "\"mobile\": \"" + user.umob + "\" }";
+				
+				// set the response type to JSON
+				res.setContentType("application/json");
+				
+				// send the JSON string back to the client as a response
+				res.getWriter().write(json);
+			}
+			else {
+				req.logout();
+				System.out.println("shit!!");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
